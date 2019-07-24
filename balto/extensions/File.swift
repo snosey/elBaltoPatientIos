dbe5cf7124ec223f2b57f6b10571c559a5c8c58a
Snosey-Mac <ahmed.el.snosey@gmail.com>
2019-07-24 14:50:56 +0200

    update

---
+++ balto/extensions/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  ElBalto
+//
+//  Created by snosey on 4/14/19.
+//  Copyright © 2019 Abanoub Osama. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let choosePopUp = Notification.Name("choosePopUp")
+    
+}
